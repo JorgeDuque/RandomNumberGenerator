7cd805226e9002870a965f519bb47a775eb7086d
Jorge Duque <jorgeduque@Jorges-MacBook-Pro.local>
2017-02-02 17:16:38 -0500

    Improved UI

--- Random Number Generator/ViewController.swift
+++ Random Number Generator/ViewController.swift
@@ -9,21 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController, UITextFieldDelegate {
-
+    
     @IBOutlet weak var textFieldMin: UITextField!
     @IBOutlet weak var textFieldMax: UITextField!
     @IBOutlet weak var labelResult: UILabel!
     
+    var titleString = ""
+    var gradientLayer: CAGradientLayer!
+    
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var backgroundView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        button.layer.borderWidth = 1.0
+        button.layer.cornerRadius = 15
+        button.layer.borderColor = UIColor(red: 189.0/255.0, green: 189.0/255.0, blue: 189.0/255.0, alpha: 1.0).cgColor
+        
+        textFieldMin.backgroundColor = UIColor(white: 0.3, alpha: 0.5)
+        textFieldMax.backgroundColor = UIColor(white: 0.3, alpha: 0.5)
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        createGradientLayer()
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
     
     @IBAction func buttonGenerateRandom(_ sender: Any) {
         
@@ -38,15 +55,37 @@ class ViewController: UIViewController, UITextFieldDelegate {
          }
         
         else if (textFieldMin.text == "" && textFieldMax.text != ""){
-            labelResult.text = "Please enter a minimum value"
+            titleString = "Min value required"
+            showAlert(titleString)
+            
         }
          else if(textFieldMin.text != "" && textFieldMax.text == ""){
-            labelResult.text = "Please enter a maximum value"
+            titleString = "Max value required"
+            showAlert(titleString)
             }
             
         else{
-            labelResult.text = "Please enter minimum and maximum values"
+            titleString = "Min and Max values required"
+            showAlert(titleString)
           }
+    }
+    
+    //Alert
+    func showAlert(_ titleStr: String){
+        let alert = UIAlertController(title: titleStr, message: "", preferredStyle: UIAlertControllerStyle.alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    //Gradient background
+    func createGradientLayer() {
+        gradientLayer = CAGradientLayer()
+        gradientLayer.frame = self.view.bounds
+        let color1 = UIColor(red: 76.0/255.0, green: 217.0/255.0, blue: 100.0/255.0, alpha: 1.0).cgColor
+        let color2 = UIColor(red: 0.0/255.0, green: 122.0/255.0, blue: 255.0/255.0, alpha: 1.0).cgColor
+        gradientLayer.colors = [color1, color2]
+        
+        self.backgroundView.layer.addSublayer(gradientLayer)
     }
     
     //Hide the keyboard
